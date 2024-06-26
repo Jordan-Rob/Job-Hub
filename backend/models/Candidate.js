9dc2333c4a94afa2cc7d2815ce74adedee8f5496
Jordan Rob <jordanrob709@gmail.com>
2024-06-26 03:26:31 +0300

    added database config & JobCandidate model

---
+++ backend/models/Candidate.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const JobCandidate = sequelize.define(
+  "Job Candidate",
+  {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    callTimeInterval: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    linkedInProfile: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    githubProfile: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    indexes: [{ unique: true, fields: ["email"] }],
+  }
+);
+
+module.exports = JobCandidate;
